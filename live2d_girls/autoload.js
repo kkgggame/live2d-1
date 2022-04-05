72b20b94e53594711d61860091ecaacc11aa917a
kkgggame <102764494+kkgggame@users.noreply.github.com>
2022-04-05 19:41:23 +0800

    Update autoload.js

--- live2d_girls/autoload.js
+++ live2d_girls/autoload.js
@@ -27,6 +27,7 @@ function loadExternalResource(url, type) {
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
+if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.min.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
@@ -34,10 +35,13 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.json",
-			//apiPath: "/live2d/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			apiPath: "https://cdn.jsdelivr.net/gh/kkgggame/live2d-1@latest/live2d/",
+			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
+}
+
+
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
