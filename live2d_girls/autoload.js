fc1bb38c96f1910f6492415ad120030e7ae83060
kkgggame <102764494+kkgggame@users.noreply.github.com>
2022-04-05 19:25:32 +0800

    Update autoload.js

--- live2d_girls/autoload.js
+++ live2d_girls/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://cdn.jsdelivr.net/gh/kkgggame/live2d-1@latest/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/kkgggame/live2d-1@latest/live2d_girls/";
 const tips_path = "/live2d_girls/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
